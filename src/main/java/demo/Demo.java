d5085e15071fcff45466fbbfa600bacbcb06c2b3
Kateryna <zaikakaterina@gmail.com>
2022-04-18 20:59:23 +0300

    Wrapping method in try - multicatch

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -3,41 +3,39 @@ package demo;
 import exceptions.WrongLoginException;
 import exceptions.WrongPasswordException;
 
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Demo {
 
-    public static void main(String[] args){
-
-        loginForm("Login_%", "Parol!", "Parol!");
-    }
-
-    public static boolean loginForm(String login, String password, String confirmPswd){
+    public static void main(String[] args) throws IOException {
         try {
-        Pattern pattern2 = Pattern.compile("^\\w+(?=.*!)|(?=!.*){1,20}$");
-        Matcher matcherPassword = pattern2.matcher(password);
-            Pattern pattern1 = Pattern.compile("^\\w{1,20}$");
-            Matcher matcherLogin = pattern1.matcher(login);
-            if (matcherLogin.find()){
-                if (matcherPassword.find()) {
-                    if (confirmPswd.equals(password)) {
-                        return true;
-                    }else{
-                    throw new WrongPasswordException("Invalid Password", "Password Error");
-                }
-                } else {
-                    throw new WrongPasswordException("Invalid Password", "Password Error");
-                }
-            } else {throw new WrongLoginException("Invalid Login", "Invalid Password", "Login Error");
-        }
+            loginForm("Login_", "Paroli", "Paroli");
         } catch (WrongLoginException e) {
             e.printStackTrace();
         } catch (WrongPasswordException e) {
             e.printStackTrace();
         }
-        return true;
     }
-}
-
+        public static boolean loginForm (String login, String password, String confirmPswd) throws IOException {
+                Pattern pattern2 = Pattern.compile("^\\w+(?=.*!)|(?=!.*){1,20}$");
+                Matcher matcherPassword = pattern2.matcher(password);
+                Pattern pattern1 = Pattern.compile("^\\w{1,20}$");
+                Matcher matcherLogin = pattern1.matcher(login);
+                if (matcherLogin.find()) {
+                    if (matcherPassword.find()) {
+                        if (confirmPswd.equals(password)) {
+                            return true;
+                        } else {
+                            throw new WrongPasswordException("Confirm Password Error", "Error");
+                        }
+                    } else {
+                        throw new WrongPasswordException("Password Error", "Error");
+                    }
+                } else {
+                    throw new WrongLoginException("Login Error", "Error", "Error");
+                }
+            }
+        }
 
